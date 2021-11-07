ae1ccc9b400db9b3500c5153869b00990a268324
JX <572673807@qq.com>
2021-11-07 15:28:38 +0800

    Update build.js

--- kbctl-ng-app/.electron-vue/build.js
+++ kbctl-ng-app/.electron-vue/build.js
@@ -34,11 +34,11 @@ async function build () {
 
   del.sync(['dist/electron/*', '!.gitkeep'])
 
-  const tasks = ['main', 'renderer']
-  const m = new Multispinner(tasks, {
-    preText: 'building',
-    postText: 'process'
-  })
+  // const maintask = ['main', 'renderer']
+  // const m = new Multispinner(maintask, {
+  //   preText: 'building',
+  //   postText: 'process'
+  // })
 
   let results = ''
 
