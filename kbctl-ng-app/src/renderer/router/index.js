2c17b94df865a5359a237364e477ed996b0576e9
JX <572673807@qq.com>
2022-01-29 17:26:08 +0800

    修改初始页面名字

--- kbctl-ng-app/src/renderer/router/index.js
+++ kbctl-ng-app/src/renderer/router/index.js
@@ -8,7 +8,7 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'landing-page',
+      name: 'wellcome-page',
       component: require('@/components/LandingPage').default
     },
     {
