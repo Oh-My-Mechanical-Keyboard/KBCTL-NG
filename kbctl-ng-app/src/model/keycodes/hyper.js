37cfe6473c9d9233f55e347fb0b35f43b0a3890d
Leo Deng <myst.dg@gmail.com>
2022-01-29 17:01:56 +0800

    拆分keycodes模型，减少重复代码

---
+++ kbctl-ng-app/src/model/keycodes/hyper.js
@@ -0,0 +1,555 @@
+export default [
+  {
+    name: '=',
+    describe: '',
+    code: 'KC_PEQL',
+    byte: 103
+  },
+  {
+    name: ',',
+    describe: '',
+    code: 'KC_PCMM',
+    byte: 133
+  },
+  {
+    name: '~',
+    describe: '',
+    code: 'KC_TILD',
+    byte: 565
+  },
+  {
+    name: '!',
+    describe: '',
+    code: 'KC_EXLM',
+    byte: 542
+  },
+  {
+    name: '@',
+    describe: '',
+    code: 'KC_AT',
+    byte: 543
+  },
+  {
+    name: '#',
+    describe: '',
+    code: 'KC_HASH',
+    byte: 544
+  },
+  {
+    name: '$',
+    describe: '',
+    code: 'KC_DLR',
+    byte: 545
+  },
+  {
+    name: '%',
+    describe: '',
+    code: 'KC_PERC',
+    byte: 546
+  },
+  {
+    name: '^',
+    describe: '',
+    code: 'KC_CIRC',
+    byte: 547
+  },
+  {
+    name: '&',
+    describe: '',
+    code: 'KC_AMPR',
+    byte: 548
+  },
+  {
+    name: '*',
+    describe: '',
+    code: 'KC_ASTR',
+    byte: 549
+  },
+  {
+    name: '(',
+    describe: '',
+    code: 'KC_LPRN',
+    byte: 550
+  },
+  {
+    name: ')',
+    describe: '',
+    code: 'KC_RPRN',
+    byte: 551
+  },
+  {
+    name: '_',
+    describe: '',
+    code: 'KC_UNDS',
+    byte: 557
+  },
+  {
+    name: '+',
+    describe: '',
+    code: 'KC_PLUS',
+    byte: 558
+  },
+  {
+    name: '{',
+    describe: '',
+    code: 'KC_LCBR',
+    byte: 559
+  },
+  {
+    name: '}',
+    describe: '',
+    code: 'KC_RCBR',
+    byte: 560
+  },
+  {
+    name: '<',
+    describe: '',
+    code: 'KC_LT',
+    byte: 566
+  },
+  {
+    name: '>',
+    describe: '',
+    code: 'KC_GT',
+    byte: 567
+  },
+  {
+    name: ':',
+    describe: '',
+    code: 'KC_COLN',
+    byte: 563
+  },
+  {
+    name: '|',
+    describe: '',
+    code: 'KC_PIPE',
+    byte: 561
+  },
+  {
+    name: '?',
+    describe: '',
+    code: 'KC_QUES',
+    byte: 568
+  },
+  {
+    name: '"',
+    describe: '',
+    code: 'KC_DQUO',
+    byte: 564
+  },
+  {
+    name: 'NUHS',
+    describe: '',
+    code: 'KC_NUHS',
+    byte: 50
+  },
+  {
+    name: 'NUBS',
+    describe: '',
+    code: 'KC_NUBS',
+    byte: 100
+  },
+  {
+    name: 'Ro',
+    describe: '',
+    code: 'KC_RO',
+    byte: 135
+  },
+  {
+    name: '¥',
+    describe: '',
+    code: 'KC_JYEN',
+    byte: 137
+  },
+  {
+    name: '無変換',
+    describe: '',
+    code: 'KC_MHEN',
+    byte: 139
+  },
+  {
+    name: '漢字',
+    describe: '',
+    code: 'KC_HANJ',
+    byte: 145
+  },
+  {
+    name: '한영',
+    describe: '',
+    code: 'KC_HAEN',
+    byte: 144
+  },
+  {
+    name: '変換',
+    describe: '',
+    code: 'KC_HENK',
+    byte: 138
+  },
+  {
+    name: 'かな',
+    describe: '',
+    code: 'KC_KANA',
+    byte: 136
+  },
+  {
+    name: 'Reset',
+    describe: '',
+    code: 'RESET',
+    byte: 23552
+  },
+  {
+    name: 'Esc `',
+    describe: '',
+    code: 'KC_GESC',
+    byte: 23574
+  },
+  {
+    name: 'LC (',
+    describe: '',
+    code: 'KC_LCPO',
+    byte: 23795
+  },
+  {
+    name: 'RC )',
+    describe: '',
+    code: 'KC_RCPC',
+    byte: 23796
+  },
+  {
+    name: 'LS (',
+    describe: '',
+    code: 'KC_LSPO',
+    byte: 23767
+  },
+  {
+    name: 'RS )',
+    describe: '',
+    code: 'KC_RSPC',
+    byte: 23768
+  },
+  {
+    name: 'LA (',
+    describe: '',
+    code: 'KC_LAPO',
+    byte: 23797
+  },
+  {
+    name: 'RA )',
+    describe: '',
+    code: 'KC_RAPC',
+    byte: 23798
+  },
+  {
+    name: 'Shift<br/>Enter',
+    describe: '',
+    code: 'KC_SFTENT',
+    byte: 23769
+  },
+  {
+    name: 'NKRO',
+    describe: '',
+    code: 'MAGIC_TOGGLE_NKRO',
+    byte: 23572
+  },
+  {
+    name: 'F13',
+    describe: '',
+    code: 'KC_F13',
+    byte: 104
+  },
+  {
+    name: 'F14',
+    describe: '',
+    code: 'KC_F14',
+    byte: 105
+  },
+  {
+    name: 'F15',
+    describe: '',
+    code: 'KC_F15',
+    byte: 106
+  },
+  {
+    name: 'F16',
+    describe: '',
+    code: 'KC_F16',
+    byte: 107
+  },
+  {
+    name: 'F17',
+    describe: '',
+    code: 'KC_F17',
+    byte: 108
+  },
+  {
+    name: 'F18',
+    describe: '',
+    code: 'KC_F18',
+    byte: 109
+  },
+  {
+    name: 'F19',
+    describe: '',
+    code: 'KC_F19',
+    byte: 110
+  },
+  {
+    name: 'F20',
+    describe: '',
+    code: 'KC_F20',
+    byte: 111
+  },
+  {
+    name: 'F21',
+    describe: '',
+    code: 'KC_F21',
+    byte: 112
+  },
+  {
+    name: 'F22',
+    describe: '',
+    code: 'KC_F22',
+    byte: 113
+  },
+  {
+    name: 'F23',
+    describe: '',
+    code: 'KC_F23',
+    byte: 114
+  },
+  {
+    name: 'F24',
+    describe: '',
+    code: 'KC_F24',
+    byte: 115
+  },
+  {
+    name: 'Exec',
+    describe: '',
+    code: 'KC_EXECUTE',
+    byte: 116
+  },
+  {
+    name: 'Help',
+    describe: '',
+    code: 'KC_HELP',
+    byte: 117
+  },
+  {
+    name: 'Menu',
+    describe: '',
+    code: 'KC_MENU',
+    byte: 118
+  },
+  {
+    name: 'Select',
+    describe: '',
+    code: 'KC_SELECT',
+    byte: 119
+  },
+  {
+    name: 'Stop',
+    describe: '',
+    code: 'KC_STOP',
+    byte: 120
+  },
+  {
+    name: 'Again',
+    describe: '',
+    code: 'KC_AGAIN',
+    byte: 121
+  },
+  {
+    name: 'Undo',
+    describe: '',
+    code: 'KC_UNDO',
+    byte: 122
+  },
+  {
+    name: 'Cut',
+    describe: '',
+    code: 'KC_CUT',
+    byte: 123
+  },
+  {
+    name: 'Copy',
+    describe: '',
+    code: 'KC_COPY',
+    byte: 124
+  },
+  {
+    name: 'Paste',
+    describe: '',
+    code: 'KC_PASTE',
+    byte: 125
+  },
+  {
+    name: 'Find',
+    describe: '',
+    code: 'KC_FIND',
+    byte: 126
+  },
+  {
+    name: 'Locking Caps Lock',
+    short: 'Lock<br/>Caps',
+    describe: '',
+    code: 'KC_LCAP',
+    byte: 130
+  },
+  {
+    name: 'Locking Num Lock',
+    short: 'Lock<br/>Num',
+    describe: '',
+    code: 'KC_LNUM',
+    byte: 131
+  },
+  {
+    name: 'Locking Scroll Lock',
+    short: 'Lock<br/>Scroll',
+    describe: '',
+    code: 'KC_LSCR',
+    byte: 132
+  },
+  {
+    name: 'Power<br/>OSX',
+    describe: '',
+    code: 'KC_POWER',
+    byte: 102
+  },
+  {
+    name: 'Power',
+    describe: '',
+    code: 'KC_PWR',
+    byte: 165
+  },
+  {
+    name: 'Sleep',
+    describe: '',
+    code: 'KC_SLEP',
+    byte: 166
+  },
+  {
+    name: 'Wake',
+    describe: '',
+    code: 'KC_WAKE',
+    byte: 167
+  },
+  {
+    name: 'Mail',
+    describe: '',
+    code: 'KC_MAIL',
+    byte: 177
+  },
+  {
+    name: 'Calc',
+    describe: '',
+    code: 'KC_CALC',
+    byte: 178
+  },
+  {
+    name: 'My<br/>Comp',
+    describe: '',
+    code: 'KC_MYCM',
+    byte: 179
+  },
+  {
+    name: 'Mouse ↑',
+    short: 'MS ↑',
+    describe: '',
+    code: 'KC_MS_UP',
+    byte: 240
+  },
+  {
+    name: 'Mouse ↓',
+    short: 'MS ↓',
+    describe: '',
+    code: 'KC_MS_DOWN',
+    byte: 241
+  },
+  {
+    name: 'Mouse ←',
+    short: 'MS ←',
+    describe: '',
+    code: 'KC_MS_LEFT',
+    byte: 242
+  },
+  {
+    name: 'Mouse →',
+    short: 'MS →',
+    describe: '',
+    code: 'KC_MS_RIGHT',
+    byte: 243
+  },
+  {
+    name: 'Mouse<br/>Btn1',
+    describe: '',
+    code: 'KC_MS_BTN1',
+    byte: 244
+  },
+  {
+    name: 'Mouse<br/>Btn2',
+    describe: '',
+    code: 'KC_MS_BTN2',
+    byte: 245
+  },
+  {
+    name: 'Mouse<br/>Btn3',
+    describe: '',
+    code: 'KC_MS_BTN3',
+    byte: 246
+  },
+  {
+    name: 'Mouse<br/>Btn4',
+    describe: '',
+    code: 'KC_MS_BTN4',
+    byte: 247
+  },
+  {
+    name: 'Mouse<br/>Btn5',
+    describe: '',
+    code: 'KC_MS_BTN5',
+    byte: 248
+  },
+  {
+    name: 'Mouse<br/>WH↑',
+    describe: '',
+    code: 'KC_MS_WH_UP',
+    byte: 249
+  },
+  {
+    name: 'Mouse<br/>WH↓',
+    describe: '',
+    code: 'KC_MS_WH_DOWN',
+    byte: 250
+  },
+  {
+    name: 'Mouse<br/>WH←',
+    describe: '',
+    code: 'KC_MS_WH_LEFT',
+    byte: 251
+  },
+  {
+    name: 'Mouse<br/>WH→',
+    describe: '',
+    code: 'KC_MS_WH_RIGHT',
+    byte: 252
+  },
+  {
+    name: 'Mouse<br/>Acc0',
+    describe: '',
+    code: 'KC_MS_ACCEL0',
+    byte: 253
+  },
+  {
+    name: 'Mouse<br/>Acc1',
+    describe: '',
+    code: 'KC_MS_ACCEL1',
+    byte: 254
+  },
+  {
+    name: 'Mouse<br/>Acc2',
+    describe: '',
+    code: 'KC_MS_ACCEL2',
+    byte: 255
+  }
+]
