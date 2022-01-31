302d0910d84a9bc7f7552c6441fafc28ed729bab
Leo Deng <myst.dg@gmail.com>
2022-01-31 16:30:03 +0800

    路由

---
+++ src/renderer/components/NavigationBar/index.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="navigation-bar">
+    <router-link
+      v-for="route in routes"
+      :key="route.name"
+      :to="route.path"
+      class="navigation-link"
+    >
+      {{ route.label }}
+    </router-link>
+  </div>
+</template>
+
+<script>
+import { routes } from '@/router/index'
+export default {
+  name: 'navigation-bar',
+  data () {
+    return {
+      routes
+    }
+  }
+}
+</script>
+
+<style scoped>
+.navigation-bar {
+  height: 48px;
+  display: flex;
+  align-items: center;
+  background-color: #222;
+}
+.navigation-link {
+  margin-left: 20px;
+  font-weight: 700;
+  text-decoration: none;
+  color: #f0f0f0;
+}
+.router-link-active {
+  color: #08f;
+  position: relative;
+}
+.router-link-active:after {
+  content: '';
+  border-top: 2px solid currentColor;
+  display: block;
+  width: 100%;
+  position: absolute;
+  top: 100%;
+}
+</style>
